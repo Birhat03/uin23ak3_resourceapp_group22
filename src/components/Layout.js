04e7e16b51bc3cc3ea0945fcc3e5652a2bac29ab
Birhat03 <birhat2003@hotmail.com>
2023-03-07 16:16:05 +0100

    gjorde om nav (med buttonsa) om til et eget komponent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,48 +1,14 @@
 import React from 'react';
-import { useState } from "react";
-import HandleSources from './Resources';
 import Header from './Header';
+import Nav from './Nav';
 
 export default function Layout() {
-    const [HTML, setState] = useState("HTML")
 
-    function StateSource(SourceStated) {
-        setState(SourceStated)
-        const btn = document.getElementById(SourceStated)
-        const HTMLbtn = document.getElementById("HTML")
-        const CSSbtn = document.getElementById("CSS")
-        const JSbtn = document.getElementById("JavaScript")
-        const ReactBtn = document.getElementById("React")
-        const SanityBtn = document.getElementById("Sanity")
-        btn.classList.add("active")
-        HTMLbtn.classList.remove("active")
-        btn.classList.add("active")
-        CSSbtn.classList.remove("active")
-        btn.classList.add("active")
-        JSbtn.classList.remove("active")
-        btn.classList.add("active")
-        ReactBtn.classList.remove("active")
-        btn.classList.add("active")
-        SanityBtn.classList.remove("active")
-        btn.classList.add("active")
-        
-
-    }
     
-
     return(
         <>
         <Header header={"Ressursarkiv"}></Header>
-        <nav>
-        <button id="HTML" className='btn active' onClick={() => StateSource('HTML')}>HTML</button>
-        <button id="CSS" className='btn' onClick={() => StateSource('CSS')}>CSS</button>
-        <button id="JavaScript"  className='btn'onClick={() => StateSource('JavaScript')}>JavaScript</button>
-        <button id="React" className='btn' onClick={() => StateSource('React')}>React</button>
-        <button id="Sanity" className='btn' onClick={() => StateSource('Sanity')}>Sanity & headless CMS</button>
-        </nav>
-        <section>
-           <HandleSources name={HTML}/>
-        </section>
+        <Nav/>
         </>
     )
 }
